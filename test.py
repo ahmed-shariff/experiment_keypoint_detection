b6dd541d47b7c1f78075347e6b4c230586583117
Ahmed Shariff <ahmed.shariff@rocketmail.com>
2019-09-23 14:18:43 +0530

    Added video support

--- test.py
+++ test.py
@@ -8,36 +8,50 @@ from torchvision import transforms
 DEVICE = 'cuda'
 
 
-def test_keypoint_rcnn():
-    img = cv2.imread("test.jpg")
-    transform = transforms.Compose([transforms.ToPILImage(),
-                                    transforms.CenterCrop(min(list(img.shape)[:2])),
-                                    transforms.Resize(512),
-                                    transforms.ToTensor()])
-    img = transform(img)
-    # torch_tensor_to_img(img, display=True)
-    model = keypointrcnn_resnet50_fpn(pretrained=True)
-    model.eval()
-    model.to(DEVICE)
-    predictions = model([img.to(DEVICE)])[0]
-    predictions_count = predictions['boxes'].shape[0]
-    img = torch_tensor_to_img(img)
-    kp_colours = {}
-    for idx in range(predictions_count):
-        # print(predictions['boxes'][idx].cpu().detach().numpy())
-        point = predictions['boxes'][idx].cpu().detach().numpy()
-        if predictions['scores'][idx] > 0.5:
-            colour = [int((255/predictions_count) * idx), 255, 255]
-            cv2.rectangle(img, tuple(point[:2]), tuple(point[2:]), tuple(colour), 1)
-            for kp_idx, kp in enumerate(predictions['keypoints'][idx]):
-                kp = kp.cpu().detach().numpy()
-                try:
-                    colour = kp_colours[kp_idx]
-                except KeyError:
-                    colour = tuple(random.randint(0, 255) for _ in range(3))
-                    kp_colours[kp_idx] = colour
-                cv2.circle(img, tuple(kp[:2].astype(np.int)), 1, colour, 1, 1)
-    display_image(img)
+def video_test():
+    cap_div = cv2.VideoCapture("/dev/video0")
+    model = TestKeypointRcnn()
+    while True:
+        r, f = cap_div.read()
+        model(f, 10)
+    cv2.destroyAllWindows()
+
+
+class TestKeypointRcnn():
+    def __init__(self, size=480):
+        self.transform = transforms.Compose([transforms.ToPILImage(),
+                                             transforms.CenterCrop(size),
+                                             transforms.Resize(512),
+                                             transforms.ToTensor()])
+        # torch_tensor_to_img(img, display=True)
+        model = keypointrcnn_resnet50_fpn(pretrained=True)
+        model.eval()
+        model.to(DEVICE)
+        self.model = model
+
+    def __call__(self, img=None, time=0):
+        if img is None:
+            img = cv2.imread("test.jpg")
+        img = self.transform(img)
+        predictions = self.model([img.to(DEVICE)])[0]
+        predictions_count = predictions['boxes'].shape[0]
+        img = torch_tensor_to_img(img)
+        kp_colours = {}
+        for idx in range(predictions_count):
+            # print(predictions['boxes'][idx].cpu().detach().numpy())
+            point = predictions['boxes'][idx].cpu().detach().numpy()
+            if predictions['scores'][idx] > 0.5:
+                colour = [int((255/predictions_count) * idx), 255, 255]
+                cv2.rectangle(img, tuple(point[:2]), tuple(point[2:]), tuple(colour), 1)
+                for kp_idx, kp in enumerate(predictions['keypoints'][idx]):
+                    kp = kp.cpu().detach().numpy()
+                    try:
+                        colour = kp_colours[kp_idx]
+                    except KeyError:
+                        colour = tuple(random.randint(0, 255) for _ in range(3))
+                        kp_colours[kp_idx] = colour
+                    cv2.circle(img, tuple(kp[:2].astype(np.int)), 1, colour, 1, 1)
+        display_image(img, time)
 
 
 def torch_tensor_to_img(image, display=False):
@@ -47,11 +61,13 @@ def torch_tensor_to_img(image, display=False):
     return image_convereted
 
 
-def display_image(image):
+def display_image(image, time=0):
     cv2.imshow("", image)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    cv2.waitKey(time)
+    if time == 0:
+        cv2.destroyAllWindows()
 
-    
+
 if __name__ == "__main__":
-    test_keypoint_rcnn()
+    # TestKeypointRcnn()()
+    video_test()
