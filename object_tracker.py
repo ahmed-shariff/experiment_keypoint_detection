af4d57d075d2797b53ffa89522493947886b064e
Ahmed Shariff <shariff.mfa@outlook.com>
2019-10-02 12:07:33 -0500

    Added the pytorch model to the object_tracking script

--- object_tracker.py
+++ object_tracker.py
@@ -11,6 +11,8 @@ import time
 import cv2
 from datetime import datetime
 
+from test import TestKeypointRcnn, torch_tensor_to_img
+
 
 # function to capture the point from the frame.
 class MouseCapture():
@@ -31,22 +33,15 @@ def main():
     mouse_capturing = MouseCapture([])
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument("-p", "--prototxt", required=True,
+    ap.add_argument("-v", "--video_source", required=False, default='/dev/video0',
+                    help="The video to use as a source")
+    ap.add_argument("-p", "--prototxt", required=False,
                     help="path to Caffe 'deploy' prototxt file")
-    ap.add_argument("-m", "--model", required=True,
-                    help="path to Caffe pre-trained model")
     ap.add_argument("-c", "--confidence", type=float, default=0.5,
                     help="minimum probability to filter weak detections")
+    ap.add_argument("-m", "--model", required=False,
+                    help="path to Caffe pre-trained model")
     args = vars(ap.parse_args())
-
-    # create the frame windows
-    cv2.namedWindow("Frame")
-    # attach the mouse call bace event to the windows
-    cv2.setMouseCallback("Frame", mouse_capturing)
-    # store the 4 points that will be prespctive transormed in the list
-
-    # Defult value for the first point for prespctive transormed
-    pts1 = np.float32([[155, 120], [480, 120], [20, 475], [620, 475]])
 
     # initialize our centroid tracker and frame dimensions
     ct = CentroidTracker()
@@ -54,53 +49,88 @@ def main():
 
     # load our serialized model from disk
     print("[INFO] loading model...")
-    net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+    out_size = 1000
+    # Defult value for the first point for prespctive transormed
+    pts1 = np.float32([[155, 120], [480, 120], [20, 475], [620, 475]])
+    pts1 = np.float32([[0, 0], [out_size, 0], [0, out_size], [out_size, out_size]])
+
+    # net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+    net = TestKeypointRcnn(920, out_size)
 
     # initialize the video stream and allow the camera sensor to warmup
     print("[INFO] starting video stream...")
-    vs = VideoStream(src=0).start()
+    vs = cv2.VideoCapture(args['video_source'])
+    # create the frame windows
+    cv2.namedWindow("Frame")
+    # attach the mouse call bace event to the windows
+    cv2.setMouseCallback("Frame", mouse_capturing)
+    # store the 4 points that will be prespctive transormed in the list
     time.sleep(2.0)
 
     # loop over the frames from the video stream
     while True:
         # read the next frame from the video stream and resize it
-        frame = vs.read()
+        r, frame = vs.read()
         result = frame
-        frame = imutils.resize(frame, width=400)
+        # frame = imutils.resize(frame, width=800)
 
         for center_position in mouse_capturing.pointList:
             # print(center_position)
             cv2.circle(frame, center_position, 5, (0, 0, 255), -1)
 
+        # # construct a blob from the frame, pass it through the network,
+        # # obtain our output predictions, and initialize the list of
+        # # bounding box rectangles
+        # blob = cv2.dnn.blobFromImage(frame, 1.0, (W, H),
+        #                              (104.0, 177.0, 123.0))
+
+        predictions, frame = net(frame)
+        rects = []
+        frame = torch_tensor_to_img(frame)
+        result = frame.copy()
+
         # if the frame dimensions are None, grab them
         if W is None or H is None:
             (H, W) = frame.shape[:2]
+            H = int(H/out_size)
+            W = int(W/out_size)
 
-        # construct a blob from the frame, pass it through the network,
-        # obtain our output predictions, and initialize the list of
-        # bounding box rectangles
-        blob = cv2.dnn.blobFromImage(frame, 1.0, (W, H),
-                                     (104.0, 177.0, 123.0))
-        net.setInput(blob)
-        detections = net.forward()
-        rects = []
+        ################################################################
+        # here where the prespective transformation should happen
+        # the following link is showing that we can apply to a point
+        #
+        ################################################################
+        if len(mouse_capturing.pointList) >= 4:
+            pts1 = np.float32([[mouse_capturing.pointList[0]],
+                               [mouse_capturing.pointList[1]],
+                               [mouse_capturing.pointList[2]],
+                               [mouse_capturing.pointList[3]]])
+            # print("point0:", [pointList[0]], "point1:", [pointList[1]], "point2:", [pointList[2]], "point3:",[pointList[3]])
+
+        pts2 = np.float32([[0, 0],
+                           [result.shape[1], 0],
+                           [0, result.shape[0]],
+                           [result.shape[1], result.shape[0]]])
+        # calculate the the prepective transformation materix.
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        # apply the transformation to the result frame
+        result = cv2.warpPerspective(frame, matrix, result.shape[:2])
 
         # loop over the detections
-        for i in range(0, detections.shape[2]):
+        for i in range(0, predictions['boxes'].shape[0]):
             # filter out weak detections by ensuring the predicted
             # probability is greater than a minimum threshold
-            if detections[0, 0, i, 2] > args["confidence"]:
+            if predictions['scores'][i] > args["confidence"]:
                 # compute the (x, y)-coordinates of the bounding box for
                 # the object, then update the bounding box rectangles list
-                box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+                box = predictions['boxes'][i].cpu().detach().numpy()
                 rects.append(box.astype("int"))
 
                 # draw a bounding box surrounding the object so we can
                 # visualize it
-                (startX, startY, endX, endY) = box.astype("int")
+                (startX, startY, endX, endY) = (box * np.array([W, H, W, H])).astype("int")
                 cv2.rectangle(frame, (startX, startY), (endX, endY),
                               (0, 255, 0), 2)
-
         # update our centroid tracker using the computed set of bounding
         # box rectangles
         objects = ct.update(rects)
@@ -114,32 +144,12 @@ def main():
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 
-            ################################################################
-            # here where the prespective transformation should happen
-            # the following link is showing that we can apply to a point
-            #
-            ################################################################
-            if len(mouse_capturing.pointList) >= 4:
-                pts1 = np.float32([[mouse_capturing.pointList[0]],
-                                   [mouse_capturing.pointList[1]],
-                                   [mouse_capturing.pointList[2]],
-                                   [mouse_capturing.pointList[3]]])
-                # print("point0:", [pointList[0]], "point1:", [pointList[1]], "point2:", [pointList[2]], "point3:",[pointList[3]])
-
-            pts2 = np.float32([[0, 0],
-                               [frame.shape[1], 0],
-                               [0, frame.shape[0]],
-                               [frame.shape[1], frame.shape[0]]])
-            # calculate the the prepective transformation materix.
-            matrix = cv2.getPerspectiveTransform(pts1, pts2)
-            # apply the transformation to the result frame
-            result = cv2.warpPerspective(frame, matrix, (W, H))
-
-            cv2.circle(result, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
-
+            
             # Print the object time, object ID , x, y
             # These points are from the orginal frame
             print(datetime.now(), ";", objectID, ";", centroid[0], ";", centroid[1])
+            cv2.circle(result, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+
 
             # THE QUESTION HOW TO TRANSFORM GET THE PREPECTIVE TRANSFORMATION FOR THE ABOVE POINTS?
             # HERE WHERE I NEED HELP WITH
@@ -156,7 +166,7 @@ def main():
 
     # do a bit of cleanup
     cv2.destroyAllWindows()
-    vs.stop()
+    # vs.stop()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -5,7 +5,10 @@ import random
 from torchvision.models.detection import keypointrcnn_resnet50_fpn
 from torchvision import transforms
 
-DEVICE = 'cuda'
+if torch.cuda.is_available():
+    DEVICE = 'cuda'
+else:
+    DEVICE = 'cpu'
 
 
 def video_test():
@@ -14,15 +17,15 @@ def video_test():
     while True:
         r, f = cap_div.read()
         # display_image(f, 10)
-        model(f, 50)
+        display_predictions(*model(f), time=50)
     cv2.destroyAllWindows()
 
 
 class TestKeypointRcnn():
-    def __init__(self, size=480):
+    def __init__(self, size=480, out_size=480):
         self.transform = transforms.Compose([transforms.ToPILImage(),
                                              transforms.CenterCrop(size),
-                                             transforms.Resize(512),
+                                             transforms.Resize(out_size),
                                              transforms.ToTensor()])
         # torch_tensor_to_img(img, display=True)
         model = keypointrcnn_resnet50_fpn(pretrained=True)
@@ -30,35 +33,39 @@ class TestKeypointRcnn():
         model.to(DEVICE)
         self.model = model
 
-    def __call__(self, img=None, time=0):
+    def __call__(self, img=None):
         if img is None:
             img = cv2.imread("test.jpg")
         img = self.transform(img)
         predictions = self.model([img.to(DEVICE)])[0]
-        predictions_count = predictions['boxes'].shape[0]
-        img = torch_tensor_to_img(img)
-        kp_colours = {}
-        for idx in range(predictions_count):
-            # print(predictions['boxes'][idx].cpu().detach().numpy())
-            point = predictions['boxes'][idx].cpu().detach().numpy()
-            if predictions['scores'][idx] > 0.5:
-                colour = [int((255/predictions_count) * idx), 255, 255]
-                cv2.rectangle(img, tuple(point[:2]), tuple(point[2:]), tuple(colour), 1)
-                x = (point[2] + point[0]) / 2
-                y = point[3]
-                cv2.circle(img, (int(x), int(y)), 1, colour, 1, 1)
-                for kp_idx, kp in enumerate(predictions['keypoints'][idx]):
+        return predictions, img
+
+
+def display_predictions(predictions, img, time=0):
+    predictions_count = predictions['boxes'].shape[0]
+    img = torch_tensor_to_img(img)
+    kp_colours = {}
+    for idx in range(predictions_count):
+        # print(predictions['boxes'][idx].cpu().detach().numpy())
+        point = predictions['boxes'][idx].cpu().detach().numpy()
+        if predictions['scores'][idx] > 0.5:
+            colour = [int((255/predictions_count) * idx), 255, 255]
+            cv2.rectangle(img, tuple(point[:2]), tuple(point[2:]), tuple(colour), 1)
+            x = (point[2] + point[0]) / 2
+            y = point[3]
+            cv2.circle(img, (int(x), int(y)), 1, colour, 1, 1)
+            for kp_idx, kp in enumerate(predictions['keypoints'][idx]):
+                continue
+                if kp_idx not in [14, 13]:
                     continue
-                    if kp_idx not in [14, 13]:
-                        continue
-                    kp = kp.cpu().detach().numpy()
-                    try:
-                        colour = kp_colours[kp_idx]
-                    except KeyError:
-                        colour = tuple(random.randint(0, 255) for _ in range(3))
-                        kp_colours[kp_idx] = colour
-                    cv2.circle(img, tuple(kp[:2].astype(np.int)), 1, colour, 1, 1)
-        display_image(img, time)
+                kp = kp.cpu().detach().numpy()
+                try:
+                    colour = kp_colours[kp_idx]
+                except KeyError:
+                    colour = tuple(random.randint(0, 255) for _ in range(3))
+                    kp_colours[kp_idx] = colour
+                cv2.circle(img, tuple(kp[:2].astype(np.int)), 1, colour, 1, 1)
+    display_image(img, time)
 
 
 def torch_tensor_to_img(image, display=False):
